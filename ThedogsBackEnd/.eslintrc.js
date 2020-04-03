1e284d209fb65c773008bb7b3b2b8cf1906a0d67
alisson <psyalisson@gmail.com>
2020-04-03 09:40:55 -0300

    Eslint

---
+++ ThedogsBackEnd/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint-config-prettier"
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  plugins:[
+    "eslint-plugin-prettier",
+    "jsx-a11y",
+  
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    
+  },
+  rules: {
+    "semi": "error",
+    "allowImplicit": true, 
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    "camelcase": "off",
+    "no-param-reassign":"off",
+    "class-methods-use-this": "off",
+    "arrow-body-style": ["error", "always"]
+  }
+};
--- ThedogsBackEnd/src/routes.js
+++ ThedogsBackEnd/src/routes.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const connection = require('./database/connection');
 
-
 const routers = express.Router();
-routers.get('/teste', async (request , response) =>{
+routers.get('/teste', async (request, response) => {
+    const { nome } = request.query;
+    console.log(nome);
 
-    const {nome} = request.query;
-    console.log(nome)
-    
-     const result = await connection('t').select('*')
-                                        .where({track : `${nome}`});
+    const result = await connection('t').select('*')
+        .where({ track: `${nome}` });
 
     return response.json(result);
 });
